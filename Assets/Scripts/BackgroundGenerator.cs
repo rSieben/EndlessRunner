59a153d34dcfe4b1f4c1b990ce23200d97693cf4
Rodrigo Sieben <rodrigo-sieben@hotmail.com.br>
2016-10-13 20:38:39 -0300

    first commit

---
+++ Assets/Scripts/BackgroundGenerator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundGenerator : MonoBehaviour {
+
+    public GameObject theSky;
+    public Transform generationPoint;
+    public float distanceBetween;
+
+    private float skyWidth;
+    
+    // Use this for initialization
+	void Start () {
+        skyWidth = theSky.GetComponent<BoxCollider2D>().size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(transform.position.x < generationPoint.position.x)
+        {
+            transform.position = new Vector3(transform.position.x + skyWidth + distanceBetween, transform.position.y, 5);
+            Instantiate(theSky, transform.position, transform.rotation);
+        }
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpForce;
+    public bool doubleJump;
+
+    private Rigidbody2D myRigidbody;
+
+    public bool grounded;
+    public LayerMask whatIsGround;
+
+    private Collider2D myCollider;
+
+    private Animator myAnimator;
+
+	// Use this for initialization
+	void Start () {
+        this.myRigidbody = GetComponent<Rigidbody2D>();
+        this.myCollider = GetComponent<Collider2D>();
+        this.myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        this.myRigidbody.velocity = new Vector2(this.moveSpeed, this.myRigidbody.velocity.y);
+        this.grounded = Physics2D.IsTouchingLayers(this.myCollider, this.whatIsGround);
+
+        if (this.grounded)
+        {
+            this.doubleJump = true;
+        }
+
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)))
+        {
+            if (this.grounded)
+            {
+                this.myRigidbody.velocity = new Vector2(this.myRigidbody.velocity.x, this.jumpForce);
+            }
+            else if(!this.grounded && this.doubleJump)
+            {
+                this.doubleJump = false;
+                this.myRigidbody.velocity = new Vector2(this.myRigidbody.velocity.x, this.jumpForce);
+            }
+        }
+
+        this.myAnimator.SetFloat("Speed",this.myRigidbody.velocity.x);
+        this.myAnimator.SetBool("Grounded", this.grounded);
+
+
+    }
+}
